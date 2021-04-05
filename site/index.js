671cb6bf5c446ee5a0c8aacaa1830ee3ac5007bb
Isaac Clayton <slightknack@gmail.com>
2021-04-05 20:35:54 +0200

    hooked editor up to backend

--- site/index.js
+++ site/index.js
@@ -1,12 +1,40 @@
-import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup"
+import {EditorView, basicSetup} from "@codemirror/basic-setup"
+import {keymap} from "@codemirror/view"
+import {EditorState, Compartment} from "@codemirror/state"
+import {defaultTabBinding} from "@codemirror/commands"
 
 import("./node_modules/playground/playground.js").then((playground) => {
-    let editor = new EditorView({
-        state: EditorState.create({
-            extensions: [basicSetup]
-        }),
-        parent: document.body
-    })
+    let tabSize = new Compartment;
 
-    playground.run("print \"Hello!\"")
+    let state = EditorState.create({
+        doc: "print \"Hello, Passerine!\"",
+        extensions: [
+            basicSetup,
+            keymap.of([defaultTabBinding]),
+            tabSize.of(EditorState.tabSize.of(4)),
+        ]
+    });
+
+    let view = new EditorView({
+        state,
+        parent: document.body,
+    });
+
+    var output = document.createElement('pre');
+    output.style = "font-family: monospace;"
+    output.innerHTML = "Click 'Run' to run...";
+    document.body.appendChild(output);
+
+    var button = document.createElement('Button');
+    button.innerHTML = "Run";
+    button.addEventListener("click", () => {
+        let result = playground.run(view.state.doc.toString());
+
+        if (result === null) {
+            output.innerHTML = "Click 'Run' to run...";
+        } else {
+            output.innerHTML = result;
+        }
+    })
+    document.body.appendChild(button);
 });
